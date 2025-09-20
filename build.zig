11b1f37082ef9ff90c2544010f22f3ef6ef57ea8
uber <uberdever@mail.ru>
2025-09-20 15:25:56 +0300

    library and test builds

--- build.zig
+++ build.zig
@@ -1,16 +1,42 @@
 const std = @import("std");
+const fs = std.fs;
 
+const builtin = @import("builtin");
+
+comptime {
+    const required_zig = "0.15.1";
+    const current_zig = builtin.zig_version;
+    const min_zig = std.SemanticVersion.parse(required_zig) catch unreachable;
+    if (current_zig.order(min_zig) == .lt) {
+        const error_message =
+            \\Sorry, it looks like your version of zig is too old. :-(
+            \\
+            \\Vetochka requires development build {}
+            \\
+            \\Please download a development ("master") build from
+            \\
+            \\https://ziglang.org/download/
+            \\
+            \\
+        ;
+        @compileError(std.fmt.comptimePrint(error_message, .{min_zig}));
+    }
+}
+
+// https://ziggit.dev/t/build-system-tricks/3531
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const sanitize = b.option(bool, "sanitize", "Enable ASan/UBSan-style flags") orelse false;
 
+    const eval_dir = "eval";
+
     const eval_sources = &.{
-        "eval/eval.c",
-        "eval/util.c",
-        "eval/memory.c",
-        "eval/encode.c",
-        "eval/native.c",
+        b.pathJoin(&.{ eval_dir, "eval.c" }),
+        b.pathJoin(&.{ eval_dir, "util.c" }),
+        b.pathJoin(&.{ eval_dir, "memory.c" }),
+        b.pathJoin(&.{ eval_dir, "encode.c" }),
+        b.pathJoin(&.{ eval_dir, "native.c" }),
     };
 
     var flags = std.ArrayList([]const u8).empty;
@@ -31,8 +57,18 @@ pub fn build(b: *std.Build) !void {
     }
     try flags.append(b.allocator, if (sanitize) "-O0" else "-O2");
 
+    const project_root = try std.fs.cwd().realpathAlloc(b.allocator, ".");
+    const cfg_text = b.fmt(
+        \\#pragma once
+        \\#define PROJECT_ROOT "{s}"
+        \\#define PATH_SEP "/"
+    , .{try std.fs.path.join(b.allocator, &.{ project_root, eval_dir })});
+    const wf = b.addWriteFiles();
+    const cfg = wf.add("config.h", cfg_text);
+    const install_cfg = b.addInstallFileWithDir(cfg, .prefix, "config.h");
+
     const lib = b.addLibrary(.{
-        .name = "eval",
+        .name = eval_dir,
         .linkage = .dynamic,
         .root_module = b.createModule(.{
             .target = target,
@@ -41,7 +77,28 @@ pub fn build(b: *std.Build) !void {
         }),
     });
     lib.root_module.addCSourceFiles(.{ .files = eval_sources, .flags = flags.items });
-    lib.root_module.addIncludePath(b.path("eval"));
+    lib.root_module.addIncludePath(b.path(eval_dir));
+    lib.root_module.addIncludePath(wf.getDirectory());
+
+    const test_sources = &.{"eval/test_eval.c"};
+    const test_exe = b.addExecutable(.{ .name = try std.fmt.allocPrint(b.allocator, "test_{s}", .{eval_dir}), .root_module = b.createModule(.{ .target = target, .optimize = optimize, .sanitize_c = .full, .link_libc = true }) });
+
+    test_exe.root_module.addCSourceFiles(.{ .files = test_sources, .flags = flags.items });
+    test_exe.root_module.addIncludePath(b.path(eval_dir));
+    test_exe.root_module.addIncludePath(wf.getDirectory());
+    test_exe.root_module.linkLibrary(lib);
+
+    switch (target.result.os.tag) {
+        .linux => test_exe.addRPath(b.path("$ORIGIN/../lib")),
+        .macos => test_exe.addRPath(b.path("@executable_path/../lib")),
+        else => {}, // On Windows, the DLL must be next to the exe or on PATH
+    }
+
+    lib.step.dependOn(&wf.step);
+    lib.step.dependOn(&wf.step);
+    test_exe.step.dependOn(&install_cfg.step);
+    test_exe.step.dependOn(&lib.step);
+
     b.installArtifact(lib);
-    b.getInstallStep().dependOn(&lib.step);
+    b.installArtifact(test_exe);
 }
