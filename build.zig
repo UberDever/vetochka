7b7ee87977a99c5dee0406bbc58125368c0f0ca4
uber <uberdever@mail.ru>
2026-04-02 12:57:10 +0300

    Added zig github workflow

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const fs = std.fs;
 
 const builtin = @import("builtin");
+const str = []const u8;
 
 comptime {
     const required_zig = "0.15.1";
@@ -25,13 +26,105 @@ comptime {
     if (@sizeOf(isize) != 8) @compileError("need 64-bit pointers");
 }
 
+const TestSuite = struct {
+    b: *std.Build,
+    c_core_dir: str,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+
+    fn init(
+        b: *std.Build,
+        c_core_dir: str,
+        target: std.Build.ResolvedTarget,
+        optimize: std.builtin.OptimizeMode,
+    ) TestSuite {
+        return .{
+            .b = b,
+            .c_core_dir = c_core_dir,
+            .target = target,
+            .optimize = optimize,
+        };
+    }
+
+    fn deinit(self: TestSuite) void {
+        _ = self;
+    }
+
+    fn makeTest(self: *TestSuite, name: []const u8, dependOn: *std.Build.Step.Compile) !*std.Build.Step.Compile {
+        const test_exe = self.b.addTest(.{
+            .name = name,
+            .root_module = self.b.createModule(.{
+                .root_source_file = self.b.path(self.b.pathJoin(&.{
+                    self.c_core_dir,
+                    std.mem.concat(self.b.allocator, u8, &[_][]const u8{ name, ".zig" }) catch unreachable,
+                })),
+                .target = self.target,
+                .optimize = self.optimize,
+            }),
+        });
+        test_exe.root_module.addIncludePath(self.b.path(self.c_core_dir));
+        test_exe.root_module.link_libc = true;
+        test_exe.root_module.linkLibrary(dependOn);
+        test_exe.step.dependOn(&dependOn.step);
+        return test_exe;
+    }
+
+    fn addTest(self: *TestSuite, name: str, lib: *std.Build.Step.Compile) !struct { *std.Build.Step.Run, *std.Build.Step.Compile } {
+        const compile_tests = try self.makeTest(try std.fmt.allocPrint(self.b.allocator, "test_{s}", .{name}), lib);
+        const run_tests = self.b.addRunArtifact(compile_tests);
+        self.b.step(
+            try std.fmt.allocPrint(self.b.allocator, "test-{s}", .{name}),
+            try std.fmt.allocPrint(self.b.allocator, "Run {s} tests", .{name}),
+        ).dependOn(&run_tests.step);
+        return .{ run_tests, compile_tests };
+    }
+
+    fn makeCFlags(self: TestSuite, sanitized: bool) !std.ArrayList([]const u8) {
+        var flags = std.ArrayList([]const u8).empty;
+        try flags.appendSlice(self.b.allocator, &.{
+            "-fPIC",
+            "--std=c99",
+            "-Wall",
+            "-Wextra",
+            "-Werror",
+            "-pedantic-errors",
+        });
+        if (!sanitized) {
+            try flags.appendSlice(self.b.allocator, &.{"-g"});
+            try flags.append(self.b.allocator, "-O2");
+        } else {
+            try flags.appendSlice(self.b.allocator, &.{ "-g", "-fno-omit-frame-pointer", "-fsanitize=address", "-shared-libasan" });
+            try flags.append(self.b.allocator, "-O0");
+        }
+        const project_root = try std.fs.cwd().realpathAlloc(self.b.allocator, ".");
+        try flags.appendSlice(self.b.allocator, &.{
+            try std.fmt.allocPrint(self.b.allocator,
+                \\-DPROJECT_ROOT="{s}"
+            , .{try fs.path.join(self.b.allocator, &.{ project_root, self.c_core_dir })}),
+            try std.fmt.allocPrint(self.b.allocator,
+                \\-DPATH_SEP="{c}"
+            , .{std.fs.path.sep}),
+        });
+        return flags;
+    }
+};
+
+fn runAndCapture(b: *std.Build, argv: []const []const u8) []const u8 {
+    var out_code: u8 = undefined;
+    const result = b.runAllowFail(argv, &out_code, std.process.Child.StdIo.Pipe) catch @panic("failed to run tool");
+    return std.mem.trim(u8, result, " \r\n\t");
+}
+
 // https://ziggit.dev/t/build-system-tricks/3531
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const sanitize = b.option(bool, "sanitize", "Enable ASan/UBSan-style flags") orelse false;
+    const cc = b.option([]const u8, "cc", "C compiler for ASan discovery") orelse "gcc-11";
 
     const c_core_dir = "reducer";
+    var s = TestSuite.init(b, c_core_dir, target, optimize);
+    defer s.deinit();
 
     const c_core_sources = &.{
         b.pathJoin(&.{ c_core_dir, "vendor_stbds.c" }),
@@ -43,31 +136,6 @@ pub fn build(b: *std.Build) !void {
         b.pathJoin(&.{ c_core_dir, "reducer_reducer.c" }),
     };
 
-    var flags = std.ArrayList([]const u8).empty;
-    defer flags.deinit(b.allocator);
-    try flags.appendSlice(b.allocator, &.{
-        "-fPIC",
-        "--std=c99",
-        "-Wall",
-        "-Wextra",
-        "-Werror",
-        "-pedantic-errors",
-    });
-    if (sanitize) {
-        try flags.appendSlice(b.allocator, &.{ "-g", "-fno-omit-frame-pointer", "-fsanitize=address", "-shared-libasan" });
-    }
-    try flags.append(b.allocator, if (sanitize) "-O0" else "-O2");
-
-    const project_root = try std.fs.cwd().realpathAlloc(b.allocator, ".");
-    try flags.appendSlice(b.allocator, &.{
-        b.fmt(
-            \\-DPROJECT_ROOT="{s}"
-        , .{b.pathJoin(&.{ project_root, c_core_dir })}),
-        b.fmt(
-            \\-DPATH_SEP="{c}"
-        , .{std.fs.path.sep}),
-    });
-
     const lib = b.addLibrary(.{
         .name = c_core_dir,
         .linkage = .dynamic,
@@ -77,54 +145,58 @@ pub fn build(b: *std.Build) !void {
             .link_libc = true,
         }),
     });
-    lib.root_module.addCSourceFiles(.{ .files = c_core_sources, .flags = flags.items });
-    if (sanitize) {
-        // Add ASan linker flags for the shared library
-        lib.root_module.addLibraryPath(.{ .cwd_relative = "/usr/lib/gcc/x86_64-linux-gnu/11" });
-        lib.root_module.addObjectFile(.{ .cwd_relative = "/usr/lib/gcc/x86_64-linux-gnu/11/libasan_preinit.o" });
-        lib.root_module.linkSystemLibrary("asan", .{});
-    }
-    lib.root_module.addIncludePath(b.path(c_core_dir));
-
-    const test_name = try std.fmt.allocPrint(b.allocator, "test_{s}", .{c_core_dir});
-    const test_exe = b.addTest(.{
-        .name = test_name,
-        .root_module = b.createModule(
-            .{
-                .root_source_file = b.path(
-                    b.pathJoin(&.{
-                        c_core_dir,
-                        std.mem.concat(b.allocator, u8, &[_][]const u8{ test_name, ".zig" }) catch unreachable,
-                    }),
-                ),
-                .target = target,
-                .optimize = optimize,
-            },
-        ),
-    });
-    test_exe.root_module.addIncludePath(b.path(c_core_dir));
-    test_exe.root_module.link_libc = true;
-    test_exe.root_module.linkLibrary(lib);
-    test_exe.step.dependOn(&lib.step);
-
-    const test_bytecode_exe = b.addTest(.{
-        .name = "test_bytecode",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path(b.pathJoin(&.{ c_core_dir, "test_bytecode.zig" })),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    test_bytecode_exe.root_module.addIncludePath(b.path(c_core_dir));
-    test_bytecode_exe.root_module.link_libc = true;
-    test_bytecode_exe.root_module.linkLibrary(lib);
-    test_bytecode_exe.step.dependOn(&lib.step);
 
     b.installArtifact(lib);
+    var test_compile_targets = std.ArrayList(*std.Build.Step.Compile).empty;
+    defer test_compile_targets.deinit(b.allocator);
+    var test_run_targets = std.ArrayList(*std.Build.Step.Run).empty;
+    defer test_run_targets.deinit(b.allocator);
 
-    const test_step = b.step("test", "Run all unit tests");
-    const run_tests = b.addRunArtifact(test_exe);
-    test_step.dependOn(&run_tests.step);
-    const run_bytecode_tests = b.addRunArtifact(test_bytecode_exe);
-    test_step.dependOn(&run_bytecode_tests.step);
+    {
+        const run, const compile = try s.addTest("reducer", lib);
+        try test_compile_targets.append(b.allocator, compile);
+        try test_run_targets.append(b.allocator, run);
+    }
+    {
+        const run, const compile = try s.addTest("bytecode", lib);
+        try test_compile_targets.append(b.allocator, compile);
+        try test_run_targets.append(b.allocator, run);
+    }
+
+    const test_all_step = b.step("test-all", "Run all unit tests");
+    for (test_compile_targets.items) |test_step| {
+        test_all_step.dependOn(&test_step.step);
+    }
+
+    // https://github.com/zigtools/zls/blob/master/schema.json
+    // https://zigtools.org/zls/guides/build-on-save/
+    const check = b.step("check", "Check if everything compiles");
+    for (test_compile_targets.items) |test_step| {
+        check.dependOn(&test_step.step);
+    }
+
+    var flags = try s.makeCFlags(sanitize);
+    defer flags.deinit(s.b.allocator);
+
+    lib.root_module.addIncludePath(b.path(c_core_dir));
+    lib.root_module.addCSourceFiles(.{ .files = c_core_sources, .flags = flags.items });
+
+    if (sanitize) {
+        const libasan_so = runAndCapture(b, &.{
+            cc,
+            "-print-file-name=libasan.so",
+        });
+        const libasan_preinit = runAndCapture(b, &.{
+            cc,
+            "-print-file-name=libasan_preinit.o",
+        });
+        const libasan_dir = std.fs.path.dirname(libasan_so).?;
+        lib.root_module.addObjectFile(.{ .cwd_relative = libasan_preinit });
+        lib.root_module.addLibraryPath(.{ .cwd_relative = libasan_dir });
+        for (test_run_targets.items) |test_step| {
+            test_step.setEnvironmentVariable("LD_PRELOAD", libasan_so);
+        }
+
+        lib.root_module.linkSystemLibrary("asan", .{});
+    }
 }
